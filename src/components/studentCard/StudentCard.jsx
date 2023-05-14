d21ca015106edf1fce8b4c16cb2ff55a8a21577d
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-05-13 19:55:43 -0500

    student card update

--- src/components/studentCard/StudentCard.jsx
+++ src/components/studentCard/StudentCard.jsx
@@ -1,38 +1,39 @@
-import photoImg from "../../assets/images/horse.jpg"
-import React from 'react'
-import "./studentCard.style.css"
+import photoImg from "../../assets/images/horse.jpg";
+import React from "react";
+import "./studentCard.style.css";
 
 const studentInfo = [
-  {src:photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard"},
-  {src:photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard"}, 
-  {src:photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard"},   {src:photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard"},
-  {src:photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard"}, 
-  {src:photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard"}
-]
+  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
+  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
+  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
+  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
+  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
+  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
+];
 
 const createStudentCard = studentInfo.map((student, index) => {
   return (
     <div className="card-container">
-        <img src={student.src} className="student-photo" alt="student-photo" key={index}/>
-        <p className="student-facts">
-          <ul>
-            <li>Name: {student.name}</li>
-            <li>Email: {student.email}</li>
-            <li>Life motto: {student.lifeMotto}</li>
-          </ul>
-        </p>
+      <img
+        src={student.src}
+        className="student-photo"
+        alt="student"
+        key={index}
+      />
+      <p className="student-facts">
+        <ul>
+          <li>Name: {student.name}</li>
+          <li>Email: {student.email}</li>
+          <li>Life motto: {student.lifeMotto}</li>
+        </ul>
         <button className="more-info">Click</button>
+      </p>
     </div>
-  )
-}
-)
+  );
+});
 
 const StudentCard = () => {
-  return (
-    <div className="cards">
-      {createStudentCard}
-    </div>
-  )
-}
+  return <div className="cards">{createStudentCard}</div>;
+};
 
-export default StudentCard
+export default StudentCard;
