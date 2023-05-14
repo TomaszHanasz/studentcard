b53186bcd6fb380f333fb2fac430ce868934ac59
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-05-13 23:25:03 -0500

    student info updated

--- src/components/studentCard/StudentCard.jsx
+++ src/components/studentCard/StudentCard.jsx
@@ -1,14 +1,62 @@
-import photoImg from "../../assets/images/horse.jpg";
+import TomPhoto from "../../assets/images/probocsis.png";
+import MuratPhoto from "../../assets/images/bocian.jpg";
+import EsmaPhoto from "../../assets/images/hedgehog.jpg";
+import ShahPhoto from "../../assets/images/lion.jpg";
+import TinkPhoto from "../../assets/images/bear.jpg";
+import AlinaPhoto from "../../assets/images/owl.jpg";
+import AidinaPhoto from "../../assets/images/cat.jpg";
 import React, { useState } from "react";
 import "./studentCard.style.css";
 
 const studentInfo = [
-  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
-  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
-  { src: photoImg, name: "tomm", email: "a@th.com", lifeMotto: "Work hard" },
-  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
-  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
-  { src: photoImg, name: "tom", email: "a@th.com", lifeMotto: "Work hard" },
+  {
+    src: TomPhoto,
+    name: "Tom",
+    email: "tom@gmail.com",
+    lifeMotto:
+      "I walk around like everything’s fine, but deep down, inside my shoe, my sock is sliding off.",
+  },
+  {
+    src: MuratPhoto,
+    name: "Murat",
+    email: "murat@gmail.com",
+    lifeMotto:
+      "I like long walks, especially when they are taken by people who annoy me.",
+  },
+  {
+    src: EsmaPhoto,
+    name: "Esma",
+    email: "esma@gmail.com",
+    lifeMotto:
+      "I remember it like it was yesterday. Of course, I don’t really remember yesterday all that well.",
+  },
+  {
+    src: ShahPhoto,
+    name: "Shahzod",
+    email: "shahzod@gmail.com",
+    lifeMotto:
+      "Truth hurts. Maybe not as much as jumping on a bicycle with a seat missing, but it hurts.",
+  },
+  {
+    src: AlinaPhoto,
+    name: "Alina",
+    email: "alina@gmail.com",
+    lifeMotto: "If I’m not back in five minutes, just wait longer.",
+  },
+  {
+    src: AidinaPhoto,
+    name: "Aidina",
+    email: "aidina@gmail.com",
+    lifeMotto:
+      "There is no sunrise so beautiful that it is worth waking me up to see it.",
+  },
+  {
+    src: TinkPhoto,
+    name: "Tink",
+    email: "tink@gmail.com",
+    lifeMotto:
+      "My mother always used to say: The older you get, the better you get, unless you’re a banana.",
+  },
 ];
 
 const createStudentCard = studentInfo.map((student, index) => {
@@ -26,7 +74,6 @@ const createStudentCard = studentInfo.map((student, index) => {
           <li>Email: {student.email}</li>
           <li>Life motto: {student.lifeMotto}</li>
         </ul>
-        <button className="more-info">Click</button>
       </p>
     </div>
   );
@@ -43,7 +90,7 @@ const StudentCard = () => {
           onMouseLeave={() => setShowMenu(false)}
           className="menuBtn"
         >
-          Show student cards
+          Students
         </button>
         {showMenu && (
           <div
